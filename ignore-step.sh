b19403a5139c3724eb07d4e11dbb12df0e4ce0e9
Andy McKay <andy.mckay@vercel.com>
2022-11-08 10:37:00 -0800

    update

--- ignore-step.sh
+++ ignore-step.sh
@@ -1,4 +1,4 @@
-#if [ "$VERCEL_GIT_COMMIT_REF" != my_branch_name ]; 
-#    then exit 1; 
-#    else exit 0; 
-#fi
+if [ "$VERCEL_GIT_COMMIT_REF" != my_branch_name ]; 
+    then exit 1; 
+    else exit 0; 
+fi
